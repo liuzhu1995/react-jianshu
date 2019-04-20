812fc52caff3466f98bdfa460c77807e8f4d9747
liuzhu <1014166283@qq.com>
2019-04-20 23:46:38 +0800

    add write page

--- src/page/write/view/write.js
+++ src/page/write/view/write.js
@@ -1,11 +1,27 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
 
 class Write extends Component {
 
   render() {
+    const {login} = this.props;
+    if(login) {
       return (
         <div>Write</div>
       )
+    }else {
+      return (
+        <Redirect to="/login"/>
+      )
+    }
+
   }
 }
-export default Write
+
+const mapStateTopPops = (state) => {
+  return {
+    login: state.getIn(["login", "login"])
+  }
+};
+export default connect(mapStateTopPops, null)(Write);
